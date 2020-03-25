74e50d9d7246ddbeaefe3f6cdd981e07d2d1151c
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-03-25 12:08:16 -0500

    adding testing

--- src/main/java/com/codeup/springblog/repositories/PostRepo.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepo.java
@@ -4,5 +4,6 @@ import com.codeup.springblog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepo extends JpaRepository<Post, Long> {
+    Post findByTitle(String title);
 
 }
